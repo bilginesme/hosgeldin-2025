df7a1f546c10a22d63f367d66605a491a07ebac6
Bilgin Esme <d_besme@borusan.com>
2024-12-20 14:21:11 +0300

    The pawn and the BG change + the balls

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const config = {
    type: Phaser.CANVAS, // Use WebGL if available, otherwise fallback to Canvas
    width: 400,        // Game width
    height: 800,       // Game height
-   backgroundColor: '#f32408', //  background
+   backgroundColor: '#aa1e22', //  background
    parent: 'game-container', // Attach to the wrapper element
    physics: {
        default: 'arcade',      // Arcade physics
@@ -25,14 +25,17 @@ const config = {
 
 // Create the Phaser game instance
 const game = new Phaser.Game(config);
+let currentScene;
 var txtDiceResult;
 var txtNumDiceTHrows;
 var boardArray = []; 
+var boardCellPositions = []; 
 const numDiceThrowsMax = 10;
 var numDiceThrows = 0;
 var isGameOver = false;
 var posPlayer = -1;
 var imgDice;
+var imgPawn;
 var isDiceRollingNow = false;
 
 const TrophyTypes = Object.freeze({
@@ -45,72 +48,37 @@ const TrophyTypes = Object.freeze({
 });
 
 function preload() {
+   currentScene = this;
+
+   this.load.image('imgDot', 'assets/dot.png');   // bunu sonra silelim
    this.load.image('myImage', 'assets/new-year-2024.png');
+   this.load.image('imgChristmasBall1', 'assets/christmas-ball-1.png');
    this.load.image('imgBoard', 'assets/board.png');
-   this.load.image('imgBG', 'assets/bg.png');
+   this.load.image('imgBG', 'assets/bg.jpg');
    this.load.image('imgWhite', 'assets/bg-white.png');
-   this.load.image('dice', 'assets/dice.png');
    this.load.audio('backgroundMusic', 'assets/SilentJungleLong.mp3'); // Key: 'backgroundMusic', Path: 'assets/music.mp3'
    for(var i = 1; i <= 6; i++) {
       this.load.image('imgDice' + i, 'assets/dice-' + i + '.png');
    }
+
+   this.load.image('imgTrophyMoney', 'assets/trophy-money.png');
+   this.load.image('imgTrophyLove', 'assets/trophy-love.png');
+   this.load.image('imgTrophyCareer', 'assets/trophy-career.png');
+   this.load.image('imgTrophyHealth', 'assets/trophy-health.png');
+   this.load.image('imgTrophyBonus', 'assets/trophy-bonus.png');
+
+   this.load.image('imgPawn', 'assets/pawn-yellow.png');
 }
 
 // Create the game (initial setup)
 function create() {
    numDiceThrows = 0;
 
-
-   this.add.text(100, 50, 'Welcome to Phaser.js!', {
-       fontSize: '12px',
-       color: '#ffffff',
-       fontFamily: 'Arial'
-   }).setOrigin(0.5);
-
    const imgWhite = this.add.image(this.scale.width / 2, this.scale.height / 2, 'imgWhite').setInteractive();;
    const imgBG = this.add.image(this.scale.width / 2, this.scale.height / 2, 'imgBG').setInteractive();;
-   imgBG.setAlpha(0.5);
 
-   const imgBoard = this.add.image(10, 190, 'imgBoard').setInteractive();
+   const imgBoard = this.add.image(10, 150, 'imgBoard').setInteractive();
    imgBoard.setOrigin(0, 0);
-
-
-   imgDice = this.add.image(260, 360, 'imgDice6').setInteractive();;
-
-   imgDice.on('pointerdown', () => {
-      if(isDiceRollingNow) {
-         console.log('Already running');
-         return;
-      }
-      
-      startDiceRollAnimation(this);
-      imgDice.setTint(0x000000); // Apply a tint to indicate press
-      imgDice.setAlpha(0.8); 
-   });
-
-   imgDice.on('pointerup', () => {
-      imgDice.clearTint();       // Remove the tint
-      imgDice.setAlpha(1);       // Reset the scale
-   });
-
-   /*
-   // Display the image at the center of the screen
-   const image = this.add.image(this.scale.width / 2, this.scale.height / 2, 'myImage').setInteractive();;
-
-   // Optional: Scale the image to fit better
-   image.setScale(0.25); // Scales the image to 50%
-
-   image.on('pointerdown', () => {
-      console.log('Button pressed!');
-      image.setTint(0x000000); // Apply a tint to indicate press
-      image.setAlpha(0.8); 
-  });
-
-  image.on('pointerup', () => {
-   image.clearTint();       // Remove the tint
-   image.setAlpha(1);       // Reset the scale
-   });
-*/
 
    const music = this.sound.add('backgroundMusic', {
    loop: true,  // Loop the music
@@ -118,25 +86,17 @@ function create() {
    });
 
     // Wait for user interaction to start audio
-   this.input.once('pointerdown', () => {
+   this.input.once('pointerdown', (pointer) => {
       // Resume the audio context
       if (this.sound.context.state === 'suspended') {
             this.sound.context.resume();
       }
 
+      console.log(pointer);
+
       // Play the music
       //music.play();
    });
-
-   // Add a visual prompt for interaction (optional)
-   /*
-   txtDiceResult = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Tap to Start', {
-      fontSize: '24px',
-      color: '#ffffff',
-      fontFamily: 'Arial'
-   }).setOrigin(0.5);
-   */
-
 
    txtDiceResult = this.add.text(300, 50, 'RESULT', {
    fontSize: '12px',
@@ -150,8 +110,13 @@ function create() {
       fontFamily: 'Arial'
       }).setOrigin(0.5);
 
-  createDice.call(this);
-  createBoardArray.call(this);
+  createDice.call();
+  createBoardArray.call();
+
+  imgPawn = this.add.image(110, 770, 'imgPawn').setInteractive();
+  imgPawn.setOrigin(0.5, 0.9);
+
+  createChristmassBalls();
 }
 
 function update(time, delta) {
@@ -159,47 +124,31 @@ function update(time, delta) {
 }
 
 function createDice() {
-   const dice = this.add.image(100, 100, 'dice').setInteractive();;
-   dice.setScale(0.25); // Scales the image to 50%
+   let scene = currentScene; // Use the global scene variable
+   imgDice = scene.add.image(270, 320, 'imgDice6').setInteractive();
 
-   dice.on('pointerdown', () => {
-      diceRoll(this);
-      dice.setTint(0x000000); // Apply a tint to indicate press
-      dice.setAlpha(0.8); 
+   imgDice.on('pointerdown', () => {
+      if(isDiceRollingNow) {
+         console.log('Already running');
+         return;
+      }
+      
+      if(numDiceThrows >= numDiceThrowsMax) {
+         alert('Game OVER');
+         return;
+      }
+
+      startDiceRollAnimation();
+      imgDice.setTint(0x000000); // Apply a tint to indicate press
+      imgDice.setAlpha(0.8); 
    });
 
-   dice.on('pointerup', () => {
-      dice.clearTint();       // Remove the tint
-      dice.setAlpha(1);       // Reset the scale
+   imgDice.on('pointerup', () => {
+      imgDice.clearTint();       // Remove the tint
+      imgDice.setAlpha(1);       // Reset the scale
    });
 }
-
-function diceRollOBSOLETE() {
-   if(isGameOver) {
-      alert('Game over');
-      return;
-   }
-
-   var diceResult = Math.ceil(Math.random() * 6);
-   console.log(diceResult);
-   txtDiceResult.setText(diceResult);
-
-   posPlayer+= diceResult;
-   if(posPlayer > (boardArray.length - 1)) {
-      posPlayer = posPlayer - (boardArray.length);
-   }
-
-   console.log('posPlayer = ' + posPlayer);
-   console.log('TROPHY = ' + getTrophyName(boardArray[posPlayer]));
-
-   numDiceThrows++;
-   txtNumDiceTHrows.setText(numDiceThrows);
-
-   if(numDiceThrows == numDiceThrowsMax) {
-      isGameOver = true;
-   }
-}
-
+ 
 function startDiceRollAnimation() {
    isDiceRollingNow = true;   
    const animationDuration = 2000; // Total duration in milliseconds
@@ -216,7 +165,8 @@ function startDiceRollAnimation() {
  
      // Update elapsed time
      elapsedTime += interval;
- 
+     imgDice.angle+= 35;
+     
      // Stop the animation after the specified duration
      if (elapsedTime >= animationDuration) {
        clearInterval(timer);
@@ -232,23 +182,42 @@ function startDiceRollAnimation() {
 }
 
 function diceRoll(diceNumber) {
+   var posPlayerPrevious = posPlayer;
+
    posPlayer+= diceNumber;
    if(posPlayer > (boardArray.length - 1)) {
       posPlayer = posPlayer - (boardArray.length);
    }
 
-   console.log('posPlayer = ' + posPlayer);
-   console.log('TROPHY = ' + getTrophyName(boardArray[posPlayer]));
-
    numDiceThrows++;
    txtNumDiceTHrows.setText(numDiceThrows);
-
 
    txtDiceResult.setText(posPlayer + ' -- ' + getTrophyName(boardArray[posPlayer]));
 
    if(numDiceThrows == numDiceThrowsMax) {
       isGameOver = true;
    }
+
+   let points = [];
+   var startPos, endPos;
+
+   if(posPlayerPrevious == -1) {
+      startPos = 0;
+      endPos = diceNumber;
+   }
+   else {
+      startPos = posPlayerPrevious + 1;
+      endPos = posPlayerPrevious + diceNumber + 1;
+   } 
+
+   for(var i = startPos; i < endPos; i++) {
+      var boardCell;
+      boardCell = boardCellPositions[i]
+      points.push({x: boardCell.x, y: boardCell.y});
+   }
+   
+   index = 0;
+   movePawn(imgPawn, points);
 }
 
 function createBoardArray() {
@@ -283,6 +252,109 @@ function createBoardArray() {
    boardArray[28] = 0;
    boardArray[29] = TrophyTypes.LOVE;
    boardArray[30] = TrophyTypes.BONUS;
+
+   boardCellPositions[0] = {x: 54, y:660 };
+   boardCellPositions[1] = {x: 102, y:654 };
+   boardCellPositions[2] = {x: 140, y:667 };
+   boardCellPositions[3] = {x: 173, y:686 };
+   boardCellPositions[4] = {x: 214, y:705 };
+   boardCellPositions[5] = {x: 259, y:713 };
+   boardCellPositions[6] = {x: 302, y:689 };
+   boardCellPositions[7] = {x: 327, y:629 };
+   boardCellPositions[8] = {x: 323, y:567 };
+   boardCellPositions[9] = {x: 285, y:523 };
+   boardCellPositions[10] = {x: 234, y:515 };
+   boardCellPositions[11] = {x: 193, y:524 };
+   boardCellPositions[12] = {x: 147, y:542 };
+   boardCellPositions[13] = {x: 104, y:556 };
+   boardCellPositions[14] = {x: 59, y:551 };
+   boardCellPositions[15] = {x: 30, y:499 };
+   boardCellPositions[16] = {x: 37, y:433 };
+   boardCellPositions[17] = {x: 77, y:388 };
+   boardCellPositions[18] = {x: 127, y:379 };
+   boardCellPositions[19] = {x: 170, y:396 };
+   boardCellPositions[20] = {x: 211, y:415 };
+   boardCellPositions[21] = {x: 261, y:430 };
+   boardCellPositions[22] = {x: 309, y:419 };
+   boardCellPositions[23] = {x: 353, y:369 };
+   boardCellPositions[24] = {x: 369, y:306 };
+   boardCellPositions[25] = {x: 352, y:247 };
+   boardCellPositions[26] = {x: 307, y:222 };
+   boardCellPositions[27] = {x: 265, y:220 };
+   boardCellPositions[28] = {x: 218, y:227 };
+   boardCellPositions[29] = {x: 168, y:223 };
+   boardCellPositions[30] = {x: 133, y:193 };
+
+   for( var i = 0 ; i < boardCellPositions.length; i++) {
+      var pos = boardCellPositions[i];
+      var trophy = boardArray[i];
+      var strImg = '';
+
+      if(trophy == TrophyTypes.MONEY) {
+         strImg = 'imgTrophyMoney';
+      }
+      else if(trophy == TrophyTypes.LOVE) {
+         strImg = 'imgTrophyLove';
+      }
+      else if(trophy == TrophyTypes.HEALTH) {
+         strImg = 'imgTrophyHealth';
+      }
+      else if(trophy == TrophyTypes.CAREER) {
+         strImg = 'imgTrophyCareer';
+      }
+      else if(trophy == TrophyTypes.BONUS) {
+         strImg = 'imgTrophyBonus';
+      }
+
+      if(strImg != '') {
+         var img = currentScene.add.image(pos.x, pos.y, strImg).setInteractive();
+         img.setOrigin(0.5, 0.9);
+      }
+   }
+}
+
+function createChristmassBalls() {
+   let scene = currentScene; // Use the global scene variable
+   imgChristmasBall1 = scene.add.image(50, -20, 'imgChristmasBall1').setInteractive();
+   imgChristmasBall1.setOrigin(0.5, 0);
+
+   imgChristmasBall2 = scene.add.image(350, -20, 'imgChristmasBall1').setInteractive();
+   imgChristmasBall2.setOrigin(0.5, 0);
+   imgChristmasBall2.scale = 0.7;
+
+   const animateBall = (ball) => {
+      const moveBall = () => {
+          const randomOffset = Phaser.Math.Between(5, 20); // Random stretch
+          const randomDuration = Phaser.Math.Between(1000, 3000); // Random duration
+          const randomAngle = Phaser.Math.Between(-3, 3); // Random swing angle
+
+          // Tween to move the ball down
+          scene.tweens.add({
+              targets: ball,
+              y: ball.y + randomOffset, // Move down by random offset
+              angle: randomAngle, // Add random swing
+              duration: randomDuration, // Use random duration
+              ease: 'Sine.easeInOut', // Smooth easing
+              onComplete: () => {
+                  // Move the ball back up with new randomness
+                  scene.tweens.add({
+                      targets: ball,
+                      y: ball.y - randomOffset, // Move back up
+                      duration: Phaser.Math.Between(1000, 3000), // Random duration for upward motion
+                      angle: -randomAngle, // Swing to the opposite direction
+                      ease: 'Sine.easeInOut',
+                      onComplete: moveBall // Loop by calling moveBall again
+                  });
+              }
+          });
+      };
+
+      moveBall(); // Start the animation
+  };
+
+  // Start the animation
+  animateBall(imgChristmasBall1);
+  animateBall(imgChristmasBall2);
 }
 
 function getTrophyName(value) {
@@ -292,4 +364,30 @@ function getTrophyName(value) {
        }
    }
    return null; // Return null if value is not found
+}
+
+let index = 0; // Define outside
+
+function movePawn(pawn, points) {
+   let scene = currentScene; // Use the global scene variable
+
+   function moveToNextPoint() {
+       if (index < points.length) {
+           let point = points[index];
+
+           scene.tweens.add({
+               targets: pawn,
+               x: point.x,
+               y: point.y,
+               duration: 1000,
+               ease: 'Power2',
+               onComplete: () => {
+                   index++; // Increment the index
+                   moveToNextPoint(); // Call recursively
+               }
+           });
+       }
+   }
+
+   moveToNextPoint();
 }
